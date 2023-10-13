ca15a3068132d8aea333637dd883f22501b8f3d3
vishalmishra-27 <133971638+vishalmishra-27@users.noreply.github.com>
2023-10-13 16:06:09 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -7,7 +7,7 @@ connectToMongo();
 const app = express()
 app.use(cors(
     {
-        origin: ["https://i-note-book-frontend.vercel.app/"],
+        origin: ["https://i-note-book-frontend.vercel.app"],
         methods: ["POST", "GET", "PUT", "DELETE"],
         credentials: "true"
     }
